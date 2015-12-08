9fc2e14f2173339ca7a7ae96d268abfae3b8d410
Connor Wiseman <wiseman.connor@gmail.com>
2015-12-08 09:41:35 -0800

    Housekeeping
    Comments/spacing in config.js, spacing in session-handler.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,17 @@
 'use strict';
 
+
+/**
+ * @namespace
+ * @property {number} port             - The port number to listen on for incoming Socket.IO connections.
+ * @property {Object} mongodb          - An object of MongoDB connection properties.
+ * @property {string} mongodb.user     - The MongoDB user.
+ * @property {string} mongodb.pass     - The MongoDB password.
+ * @property {string} mongodb.host     - The MongoDB host.
+ * @property {number} mongodb.port     - The MongoDB port.
+ * @property {string} mongodb.database - The MongoDB database.
+ * @function mongodb.dbURI             - A utility function that returns a MongoDB connection URI.
+ */
 var config = module.exports = {
     port: 1024,
     mongodb: {
@@ -11,21 +23,27 @@ var config = module.exports = {
         dbURI: function() {
             var dbURI = 'mongodb://';
 
+
             if (typeof this.user !== 'undefined' && this.user !== null) {
                 dbURI += this.user + ':' + this.pass + '@';
             }
 
+
             dbURI += this.host;
+
 
             if (typeof this.port !== 'undefined' && this.port !== null) {
                 dbURI += ':' + this.port;
             }
 
+
             dbURI += '/';
+
 
             if (typeof this.database !== 'undefined' && this.database !== null) {
                 dbURI += this.database;
             }
+
 
             return dbURI;
         }
--- server/middleware/session-handler.js
+++ server/middleware/session-handler.js
@@ -1,14 +1,18 @@
 'use strict';
 
+
 var Session = require('../controllers/session-controller.js');
+
 
 module.exports = function(socket, next) {
     var session_id = socket.handshake.query['taka-sessionid'];
+
 
     var ip_address = (socket.request.headers && socket.request.headers['x-forwarded-for'])
         || socket.request.ip 
         || socket.request._remoteAddress 
         || (socket.request.connection && socket.request.connection.remoteAddress);
+
 
     socket.session = {
         id:             session_id,
@@ -17,11 +21,15 @@ module.exports = function(socket, next) {
         role:           'guest'
     };
 
+
     Session.start(session_id, function(error, session) {
         if (error) {
             return next(error);
         }
+
+
         socket.emit('session', session);
+
 
         return next();
     });
